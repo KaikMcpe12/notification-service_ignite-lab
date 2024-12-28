026d7c9a593da9ff21ee8c95955f818c675eb618
Kaik <119709479+KaikMcpe12@users.noreply.github.com>
2024-12-28 11:42:26 -0300

    :sparkles: feat: added test, prisma respositorie and routes

---
+++ src/application/use-cases/get-recipient-notifications.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notification-repository';
+import { Notification } from '@application/entities/notification';
+
+interface GetRecipientNotificationRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotifications {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: GetRecipientNotificationRequest,
+  ): Promise<GetRecipientNotificationResponse> {
+    const { recipientId } = request;
+
+    const notifications =
+      await this.notificationRepository.findManyByRecipientId(recipientId);
+
+    return {
+      notifications,
+    };
+  }
+}
--- src/infra/databases/database.module.ts
+++ src/infra/databases/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prismsa.service';
-import { NotificationsRepository } from 'src/application/repositories/notification-repository';
+import { NotificationsRepository } from '@application/repositories/notification-repository';
 import { PrismaNotificationRepository } from './prisma/repositories/prisma-notification-repository';
 
 @Module({
